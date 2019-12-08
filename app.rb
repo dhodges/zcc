452450bdf62efa83d3a8b320489abb66aad4bb76
David Hodges <hodges.david@gmail.com>
2019-12-09 06:52:14 +1100

    cmdline option --jsondir, to source json files (organization/ticket/user) from elsewhere

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
+require 'optparse'
+require 'ostruct'
 require 'require_all'
+
+def gather_cmd_line_opts
+    options = OpenStruct.new
+    OptionParser.new do |opt|
+      opt.on('--jsondir JSON_DIRECTORY_PATH', 'The directory containing the initial json files') { |o| options.jsondir = o }
+      options.jsondir ||= __dir__
+    end.parse!
+    options
+end
 
 # will not run if this file was loaded or required
 if __FILE__==$0
     require_all __dir__+'/src'
-    Navigator.new(Loader.load_json_data).loop_for_input
+
+    opts = gather_cmd_line_opts
+    Navigator.new(Loader.load_json_data(opts.jsondir)).loop_for_input
 end
