39ead1c07f59fd5e5793c6ea9d3d46eeeefc7088
David Hodges <hodges.david@gmail.com>
2019-12-08 17:45:30 +1100

    display: special treatment for boolean and array values

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -18,6 +18,72 @@ describe Display do
         it 'max_key_length is accurate' do
             expect(Display.max_key_length(ticket_results)).to eq(15)
         end
+
+        let(:formatted_key) { Display.format_key('erewhon', 10) }
+
+        it 'keys are padded with the expected number of spaces' do
+            expect(formatted_key).to eq('erewhon   ')
+        end
+        
+        let(:formatted_line) { Display.format_line('barsoom', 10, 'yes') }
+
+        it 'lines have a padded key and a value' do
+            expect(formatted_line).to eq('barsoom   yes')
+        end
+    end
+
+    describe 'when formatting a result' do
+        let(:search_path) { ['ticket', 'organization_id'] }
+        let(:search_term) { 113 }
+        let(:finder) { Finder.new(fixture_data) }
+        let(:ticket) { finder.locate(search_path, search_term).first }
+        let(:formatted_ticket) { Display.prettify([ticket]) }
+
+        # chosen because this ticket's fields have booleans, arrays, numbers and strings
+        it 'the prettified results look OK' do
+            expect(formatted_ticket).to eq(
+"""subject          A Problem in Tonga
+-------          
+_id              b4875dbc-c167-4625-a1e4-d14ed409c62c
+assignee_id      31
+assignee_name    Jessica Raymond
+created_at       2016-04-22T12:55:29 -10:00
+description      Proident est ea duis eiusmod. Deserunt laboris eu cupidatat culpa tempor aliquip eu.
+due_at           2016-08-06T07:41:47 -10:00
+external_id      0a8d9bab-b265-4801-8d64-ba6cba3df967
+has_incidents    no
+organization_id  113
+priority         urgent
+status           open
+submitter_id     73
+submitter_name   Moran Daniels
+tags             Michigan, Florida, Georgia, Tennessee
+type             question
+url              http://initech.zendesk.com/api/v2/tickets/b4875dbc-c167-4625-a1e4-d14ed409c62c.json
+via              voice
+""")
+        end
+    end
+
+    describe 'when searching a boolean field' do
+        let(:finder) { Finder.new(fixture_data) }
+        let(:search_path) { ['user', 'shared'] }
+        let(:users) { finder.locate(search_path, search_term) }
+        let(:formatted_value) { Display.format_value('shared', 10, users.first['shared']) }
+
+        describe 'that is false' do
+            let(:search_term) { false }
+            it 'the result is "no"' do
+                expect(formatted_value).to eq 'no'
+            end
+        end
+
+        describe 'that is true' do
+            let(:search_term) { true }
+            it 'the result is "yes"' do
+                expect(formatted_value).to eq 'yes'
+            end
+        end
     end
 
     describe 'when ordering results' do
--- src/display.rb
+++ src/display.rb
@@ -16,10 +16,10 @@ class Display
         end
 
         def prettify(results)
-            format_str = "%-#{max_key_length(results)}s"
+            max_key_length = max_key_length(results) + 2
             results.map do |result|
                 preferred_order(result.keys).map do |key|
-                    "#{sprintf(format_str, key)}  #{result[key] || ''}"
+                    format_line(key, max_key_length, result[key])
                 end.join("\n") + "\n"
             end.join("\n")
         end
@@ -40,6 +40,32 @@ class Display
             end
 
             outkeys.union(inkeys.filter {|k| !outkeys.include?(k)}.sort)
+        end
+
+        def format_line(key, max_key_length, value) 
+            format_key(key, max_key_length) + format_value(key, max_key_length, value)
+        end
+
+        def format_value(key, max_key_length, value)
+            case value
+            when Array
+                if key.include?('tickets')
+                    value.join("\n" + format_key('', max_key_length))
+                else
+                    value.join(', ')
+                end
+            when FalseClass
+                'no'
+            when TrueClass
+                'yes'
+            else
+                value.to_s
+            end
+        end
+
+        # pad the key with spaces on the right
+        def format_key(key, max_key_length)
+            sprintf("%-#{max_key_length}s", key)
         end
     end
 end
