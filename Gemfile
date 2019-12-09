e8353609550cbf89d5ed97acc92728ef35ebd844
David Hodges <hodges.david@gmail.com>
2019-12-09 12:58:33 +1100

    cmdline option --profile, to generate runtime profiling stats

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 gem 'require_all'
 gem 'rb-readline'
+gem 'ruby-prof'
 gem 'tty-pager'
 
 group :development, :test do
--- app.rb
+++ app.rb
@@ -1,12 +1,16 @@
+#!/usr/bin/env ruby
+
 require 'optparse'
 require 'ostruct'
 require 'require_all'
+require 'ruby-prof'
 
 def gather_cmd_line_opts
     options = OpenStruct.new
     OptionParser.new do |opt|
       opt.on('--jsondir JSON_DIRECTORY_PATH', 'The directory containing the initial json files') { |o| options.jsondir = o }
       options.jsondir ||= __dir__
+      opt.on('--profile', 'capture profiling stats') { |o| options.profile = true }
     end.parse!
     options
 end
@@ -16,6 +20,16 @@ if __FILE__==$0
     require_all __dir__+'/src'
 
     opts = gather_cmd_line_opts
+    RubyProf.start if opts.profile
+
+    at_exit do
+      if opts.profile
+        result = RubyProf.stop
+        printer = RubyProf::GraphHtmlPrinter.new(result)
+        printer.print(File.new __dir__+'/profile.html', 'w')
+      end
+    end
+
     Navigator.new(Loader.load_json_data(opts.jsondir)).run
 
     puts 'bye!'
